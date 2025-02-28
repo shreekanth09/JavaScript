c4c87d1550d885e3a4a775a508c4cc37a5fad9d8
Shreekanth_09 <shrikanths188@gmail.com>
2025-02-28 21:30:58 +0530

    Add files via upload

---
+++ map_function/map.js
@@ -0,0 +1,68 @@
+
+                          //MAP FUNCTION
+// let arr=[1,2,3,4,5,6];
+// let newArr=arr.map((sum)=>{
+//    return sum;
+// });
+// console.log(newArr);
+
+
+// let numbers=[2,4,6,8,10];
+// let square=numbers.map((val)=>{
+//     return val*val;
+// });
+// console.log(square);
+                           //filter function
+
+// let num=[1,2,3,4,5,6,7,8,9,10];
+// let evnar=num.filter((val)=>{
+//     return val % 2 ===0;
+// });
+// console.log(evnar);
+
+// let arr=[1,2,3,4,5,6,7,7,8,9,10];
+// let newArr=arr.filter((val)=>{
+//     return val >8;
+// });
+// console.log(newArr);
+
+                                //reduce method in fumction
+//   let arr=[10,20,30];
+//   let newarr=arr.reduce((res,curr)=>{
+//     return res + curr;
+//   });
+//   console.log(newarr);     
+  
+//   let array=[1,2,3,4,5,6];
+//   let newArr=array.reduce((res,cur)=>{
+//     return (res +cur)*2;
+//   });
+//   console.log(newArr);
+//   //ans= 208 _think logically shreekanth
+// // finding the largest numbers among numbers
+
+// let num=[1,20,3,51,2];
+// let newNum=num.reduce((pre,cur)=>{
+//     // return pre>cur?pre:cur;  //GREATER
+//     return pre<cur?pre:cur;   // LESSOR
+// });
+// console.log(newNum);
+
+//PRACties qns among this three methods
+
+// let arr=[40,50,100,99,95,90,67];
+// let above=arr.filter((val)=>{
+//     return val>90;
+// });
+// console.log(above);
+
+// let n=prompt("entre numbers");
+// let arr=[];
+// for(let i=1; i<=n; i++){
+//     arr[i-1]=i;
+// }
+// console.log(arr);
+// let newArr=arr.reduce((res,cur)=>{
+//     return res * cur;
+// });
+// console.log(newArr);
