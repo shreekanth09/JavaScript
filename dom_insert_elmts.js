35d85ea0aeaeb4a63d7690cded94bfff1987ca79
Shreekanth_09 <shrikanths188@gmail.com>
2025-03-02 21:47:20 +0530

    Add files via upload

---
+++ dom_insert_elmts.js
@@ -0,0 +1,23 @@
+let btn=document.createElement("button");
+btn.innerText=" button";
+console.dir(btn);
+
+let div=document.querySelector("div");
+ div.append(btn);
+// let plc=document.createElement("button");
+// plc.innerText="hello bro";
+// console.log(plc);
+
+// let res=document.querySelector("div");
+// res.after(  plc);
+
+// let head=document.createElement("h1");
+// head.innerHTML=("<i>SHREEKANTH</i>");
+
+// let newval=document.querySelector('body');
+// newval.prepend(head);
+
+// let shi=document.querySelector("p");
+// console.log(shi);
+// shi.remove();
+// head.remove();
