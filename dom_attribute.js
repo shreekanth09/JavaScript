14b8b271078e61524c7fc85290767d1e2c329d59
Shreekanth_09 <shrikanths188@gmail.com>
2025-03-02 21:46:57 +0530

    Add files via upload

---
+++ dom_attribute.js
@@ -0,0 +1,14 @@
+// let div=document.querySelector("div");
+// console.log(div);
+// let res=div.getAttribute('name');
+// console.log(res);
+// let para=document.querySelector("p");
+// console.log(para.getAttribute("class"));
+
+
+// let old =document.querySelector("p");
+// console.log(old.setAttribute("class","prashanth"));
+// let div=document.querySelector("div");
+// div.style.backgroundColor="yellow";
+// div.style.fontWeight=900;
+// div.style.textTransform="uppercase";
