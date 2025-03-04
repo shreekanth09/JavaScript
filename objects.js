586a11612cdb5abbfbbb805b8c5cfba59c6a5578
Shreekanth_09 <shrikanths188@gmail.com>
2025-03-04 21:06:40 +0530

    Add files via upload

---
+++ objects.js
@@ -0,0 +1,44 @@
+// let one={
+   
+//    calcTax(){
+// console.log("Tax is 10%");
+//    }
+// };
+
+// let shreekanth={
+//     salary:50000,
+// };
+// let shreekanth2={
+//     salary:50000,
+// };
+// let shreekanth3={
+//     salary:50000,
+//     calcTax(){
+//         console.log("Tax is 20%");
+//            }
+// };
+// let shreekanth4={
+//     salary:50000,
+// };
+// shreekanth.__proto__=one;
+// shreekanth2.__proto__=one;
+// shreekanth3.__proto__=one;
+// shreekanth4.__proto__=one;
+// // shreekanth5.__proto__=one;
+
+// let touch=document.querySelector("#cls");
+// let body=document.querySelector("body");
+// touch.addEventListener("click",()=>{
+//   console.log(touch.innerText="Pu");
+//   touch.style.backgroundColor="black";
+//   body.style.backgroundColor="red";
+// });
+// touch.addEventListener("dblclick",()=>{
+//     console.log(touch.innerText="SSLC");
+//     touch.style.backgroundColor="BLUE";
+//     body.style.backgroundColor="green";
+//   })
+let x=[1,2,3];
+x.append([4,5]);
+// console.log(x);
+print(len(x))
