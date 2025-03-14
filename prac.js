ca9280a19fa02c54703d9e4468e2f4a4fb0716f7
Shreekanth_09 <shrikanths188@gmail.com>
2025-03-14 23:17:09 +0530

    Add files via upload

---
+++ prac.js
@@ -0,0 +1,291 @@
+// // let name="abc".toUpperCase()
+// // let a=[1,2,3].push(5);
+// // console.log(a);
+// // sk="1","2","3";
+// // sk[2];
+
+// // x=10;
+// // y=20;
+// // function sk(){
+// //     // x+y;
+// //     return x+y;
+// // }
+// // console.log(sk());
+// //  x=prompt("enter you number");
+// // if(x==1)
+// //     console.log("you are true ..")
+
+// // else
+// // console.log("you are false");
+
+
+// // function shree(msg){  // PARAMETER
+// //     console.log(msg);
+// // }
+// // shree("shreekanth"); // Argumente
+
+
+// // function calculate(a,b){
+// //     // console.log(a+b);
+// //     sum=a+b;
+// //     return sum;
+// // }
+// // // calculate(10,20);
+// // let val=calculate(2,4);
+// // console.log(val);
+
+
+// // function multy(a,b){
+// //     val=a*b;
+// //     // val=a+b;
+// //     console.log(val);
+// // }
+// // multy(10,12);
+
+// // function mane(){
+// //     let one=prompt("enter any number ");
+// //     if(one==1){
+// //         console.log("one");
+// //     }
+// // }
+// // mane();
+
+
+// // function divid(ans){
+// //     console.log(ans);
+// // }
+// // divid("lokesh");
+
+// // function sub(ab,ba){
+// //     sum=ab%ba;
+// //     console.log(sum);
+// // }
+// // sub(10,2.2);
+
+
+// // ARROW FUNCTIONS
+//  let num=(a,b)=>{
+//     // console.log(a,b);
+//     return a+b;
+//  }
+
+
+
+// //  let sum=(a,b)=>{
+// //     na=a+b;
+// //     va=a*b;
+// //     console.log(a+b);
+// //     console.log(a*b);
+// //  }
+// //  sum(10,20);
+
+// //   function sum(a,b){
+// //     na=a+b;
+// //     va=a*b;
+// //     console.log(a+b);
+// //     console.log(a*b);
+// //  }
+// //  sum(10,20);
+
+//     //   USING FOR LOOP
+// // let myName=()=>{
+// //     let n=9;
+// //     for(let idx=0 ;idx<=n; idx++)
+// //         console.log("shreekanth");
+// // };
+// // myName();
+
+//     // USING FOR EACH LOOP;;
+// // let myName=()=>{
+// //     let n=10;
+// //     for()
+// // }
+
+
+// // function shiva(){
+// //    Engg= "shreekanth";
+// //    return Engg;
+// // // console.log(uday);
+// // }
+// // let uday=shiva();
+// // console.log(uday);
+
+// // let uday=()=>{
+// //     let n=1;
+// //    for(let i=0;i<=n; i++){
+// //     nans="Shivaprasad\nUdayShankar\nShreekanth";
+// //    }
+// //    return nans;
+// // }
+// // let shiva=uday();
+// // console.log(shiva);
+
+// // let n=1,i=1;
+// // while(i<=n){
+// //     console.log("Shivaprasad\nUdayShankar\nShreekanth\nYashu");
+// //     i++;
+// // }
+
+// // function sk(a,b,c,d){
+// //    s=(a+b*c)/d;
+// //    shree=prompt("do you want to sea what you calculated",s);
+// // }
+// // sk(10,20,30,40);
+// // console.log(shree);
+
+// // let nanu=(a,b)=>{
+// //   res=a*b;
+// //   return res;
+// // };
+  
+
+// // let shiv=["Shreekanth","Shiva","Uday","Prashanth"];
+// // for(let i=0 ; i<shiv.length;i++){
+// //   console.log(shiv[i]);
+// // }
+
+// let cities=["Bengaluru","Mysuru","Hassan","Tumkru"];
+// //  for(let city of cities){
+// //   console.log(city);
+// //  }
+// // console.log("\n");
+// //  let courses=["CSE","ISE","AIML","ECE"];
+// //  for(let nams of courses){
+// //   console.log(nams);
+// //  }
+// //   let subjects=["Maths","Science","Social Science","Kannada"];
+// //   for(let sub of subjects){
+// //     console.log(sub);
+// //   }
+// // let marks=[85,97,44,37,76,60];
+// // let sum=0;
+// // for(let val of marks){
+// //   // console.log(val);
+// // sum+=val;
+// // // sum=sum/6;
+// // }
+// // let avg=0;
+// //   console.log(sum);
+// //   avg=sum/6;
+// //   console.log(avg);
+
+
+// // let arr=["shreekanth","Prashanth"];
+// // for(let val of arr){
+// //   console.log(val.toUpperCase());
+// // }
+// // arr.forEach((val)=>{
+// //   console.log(val);
+// // });
+ 
+// // function sk(a,b){
+//   // s=a*b;
+//   // console.log(s);
+
+// // 
+// // let number=[10,2,3,6,8];
+// // number.forEach((val)=>{
+// //   console.log(val*val);
+// // }
+// // );
+// // values=[10,20,30,40];
+// // let sum=0;
+// //   for(let val of values){
+// //     console.log(val);
+// //     sum+=val;
+    
+// //   }
+// //   console.log(sum);
+// //   let avg=0;
+// //   avg=sum/4;
+// //   console.log(avg);
+  
+// // values.forEach((val)=>{
+// //   console.log(val);
+// // });
+// // function myName(a,b){
+// //  s= a+b;
+// //  console.log(s);
+// // }
+// // myName(10,20);
+// // myName(10,20);
+// // let final=(a,b)=>{
+// // res=a*b
+// // console.log(res);
+// // }
+// // final(10,20);
+
+// let arr=[20,30,20,40];
+// // for(let res of arr){
+// //   console.log(res);
+// // }
+// arr.forEach((val)=>{
+// console.log(val);
+// });
+// let i=0;;
+// while(i<=5){
+//   console.log("shreekanth");
+//   i ++;
+// }
+// let id=document.querySelector(".class");
+// console.log(id);
+// // let val=id.getAttribute("id");
+// // // console.log(val);
+// // let val=id.getAttribute("id");
+// // console.log(val);
+// let res=id.getAttribute("id");
+// console.log(res);
+// function myName(a,b){
+//     a=10;
+//     b=20;
+//     s=a+b;
+//     console.log(s);
+// }
+// myName();
+
+// let shree=()=>{
+//     a=20;
+//     b=23;
+//     c=a*b;
+//     console.log(c);
+// }
+// let arr=[10,20,30,40];
+
+// for(let val of arr){
+//     console.log(val);
+// }
+// // arr.forEach((val) =>
+// // {
+// //  console.log(val);
+// })
+
+// function shreekanth(a,b){
+//     c=(a) + b;
+//     console.log(c);
+// }
+// shreekanth("shreekanth\n","shiva");
+
+// let shreekanth=(a,b)=>{
+//    c=a&&b,a||b;
+//    console.log(c);
+// }
+// shreekanth(3,10);
+// let mySon=(a,b)=>{
+//    let s=a+b;
+//    console.log(s);
+// }
+// mySon(10,30);
+// let arr=[10,29,49];
+// for(let one of arr){
+//    console.log(one);
+// }
+// arr.forEach((val)=>{
+//    console.log(val);
+// });
+
+// let n=10;
+// let i=0;
+// while(i<=n){
+//    console.log("shreekanth\n");
+//    i++;
+// }
