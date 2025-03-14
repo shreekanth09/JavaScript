735667fcbc3a628c1583a9a134fb77cb2d9b46ef
Shreekanth_09 <shrikanths188@gmail.com>
2025-03-14 23:16:37 +0530

    Add files via upload

---
+++ sec_prg.js
@@ -0,0 +1,87 @@
+// function countsk(str){
+//  let count=0;
+//  for(const char of str){
+//     // console.log(char);
+//     // for(let char=0; char<; char++){
+//     if(char==="a" || char==="e" || char==="i" || char==="o" || char==="u"){
+//         count++;
+//     }
+//     // console.log(count);
+// }
+// console.log(count);
+//  }
+    //PRIME NUMBER
+   // let n;
+   //let n;
+//    let num=prompt("enter the number:");
+//    for( n=0;n<num ; n++){
+//     if(n/n===0 && n/n===1){
+//         console.log(n);
+//     }
+// }
+
+//  const mult=(a,b)=>{
+//     sum=(a*b);
+//     return sum;
+//  }
+//  let val=mult(3,5);
+//  console.log(val);
+
+//  const multi=(y,x)=>{
+//    sum= (y*x);
+// //   sum=multi(3,8);
+//    console.log(sum);
+// //    return sum;
+//  }
+//  sum=multi(3,10);
+// //  console.log(sum);
+ 
+
+  // FOReACH LOOP IN JS
+  // let arr=[10,20,2,3,4];
+  // arr.forEach(function print(val){
+  //   console.log(val);
+  // })
+  // let name=["shreekanth","shiva","uday","prashanth"];
+  // name.forEach(function nms(val){
+  //   console.log(val)
+  // })
+  // // finding the squareroot using foreach loop
+  // let sqrs=[2,3,5,7,8];
+  // sqrs.forEach((val)=>{
+  //   console.log(val*val);
+  // })
+       // using filter function
+  // let arr=[20,90,93,50,60];
+  //  const output=arr.filter((val)=>{
+    
+  //   return val>=90;
+  //  });
+  //  console.log(output);
+
+//   let n=prompt("enter the number");
+//   let arr=[];
+//    for(let i=1; i<=n; i++ ){
+//     arr[i-1]=i;
+//    }
+//    console.log( arr);
+//    const sum=arr.reduce((pre,cur)=>{
+// return pre+cur;
+//   })
+//   console.log(sum);
+//   // console.log(sum*n);
+//   const pro=arr.reduce((pre,cur)=>{  // this is also example for factorial program
+//   return pre*cur;
+//   })
+// console.log(pro);
+
+  let n=prompt("enter the guessing number:");
+  //for(let i=0;i<=n;i++){
+   if(n===10){
+    console.log("ur quessing right number :");
+   }
+   let  q=prompt("entet the guessing number:");
+   if(n===20){
+    console.log("ur answer will be ");
+   }
+  
