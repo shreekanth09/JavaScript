330dba0faf1dc4ece7544085c56296a02bb7c9b2
Shreekanth_09 <shrikanths188@gmail.com>
2025-04-04 22:05:33 +0530

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,3 @@
+const movements = [10, 20, 30, 40, -10];
+const newOne = movements.reduce((acc, cur) => acc + cur); //it is used to perform form task it would be mathametical or other
+console.log(newOne);
