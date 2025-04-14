245d7e5644763d110ef29650f71e531391eb6e27
Shreekanth_09 <shrikanths188@gmail.com>
2025-04-14 21:49:03 +0530

    Add files via upload

---
+++ swithc.js
@@ -0,0 +1,16 @@
+let day="friday";
+switch (day) {
+    case "monday":
+        console.log("hi today is monday");
+        break;
+    case "tuesday":
+        console.log("this is tuesday");
+        break;
+    case "friday":
+        console.log("this is friday");
+        break;        
+
+    default:
+        console.log("this is not valide");
+        break;
+}
