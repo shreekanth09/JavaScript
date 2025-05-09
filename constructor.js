01514b246b56ecb58439e2d28875428285af51b1
Shreekanth_09 <shrikanths188@gmail.com>
2025-05-09 23:09:51 +0530

    Add files via upload

---
+++ constructor.js
@@ -0,0 +1,8 @@
+const Info = function (name, usn) {
+  this.nameIs = name;
+  this.usnIs = usn;
+};
+const sk = new Info("shreekanth", "4RA22CS095");
+const sp = new Info("shivaPrasad", "4RA22CS094");
+const us = new Info("udayShanker", "4RA22CS110");
+console.log(sk, sp, us);
